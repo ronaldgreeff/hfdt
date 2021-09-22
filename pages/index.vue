dab65e962737297026f2efef6d42eef0f8bae2d4
Ronald Greeff <ronald.greeff@gmail.com>
2021-09-22 17:20:00 +0100

    index ready to be added to

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,14 @@
 <template>
-  <div class="container">
-    <div>
-      <Thirdway />
-      <TXoLogo />
-      <TXoLogo2 />
-      <h1 class="title">hf-devtest</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    <Navbar />
   </div>
 </template>
 
 <script>
-export default {}
+import Navbar from '@/components/Navbar'
+export default {
+  Navbar,
+}
 </script>
 
 <style></style>
