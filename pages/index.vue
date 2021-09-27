762355fe8463f83e9e502b7ccdb8d123d30af4e5
Ronald Greeff <ronald.greeff@gmail.com>
2021-09-27 12:44:11 +0100

    added vue-flickity for carousel

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <Navbar v-bind:navContacts="navContacts" />
-    <IntroHeroText />
-    <IntroDescription />
+    <IntroHeroText v-bind:introHeroText="introHeroText" />
+    <IntroDescription v-bind:introDescription="introDescription" />
+    <MainCarousel v-bind:carouselData="carouselData" />
     <MainSubscribe />
   </div>
 </template>
@@ -24,14 +25,14 @@ export default {
   data(){
     return {
       navContacts: [
-        [{type: 'title', value: 'enquiries'}, {type: 'title', value: 'general'}, {type: '', value: '+44 (0) 020 3613 4733'}, {type: 'email', value: 'info@txowork.com'},],
-        [{type: 'title', value: ''}, {type: 'title', value: 'sales'}, {type: '', value: '+44 (0) 020 3613 4733'}, {type: 'email', value: 'info@txowork.com'},],
-        [{type: 'title', value: 'address'}, {type: 'title', value: 'morelands'}, {type: 'text', value: '5-23 Old Street'}, {type: 'text', value: 'London EC1V 9HL'},],
-        [{type: 'title', value: 'connect'}, {type: 'link', value: 'instagram', url: 'www.instagram.com'}, {type: 'link', value: 'linkedin', url: 'www.linkedin.com'}, {type: 'link', value: 'facebook', url: 'www.facebook.com'},],
+        [{type: 'colTitle', value: 'enquiries'}, {type: 'title', value: 'general'}, {type: '', value: '+44 (0) 020 3613 4733'}, {type: 'email', value: 'info@txowork.com'},],
+        [{type: 'colTitle', value: ''}, {type: 'title', value: 'sales'}, {type: '', value: '+44 (0) 020 3613 4733'}, {type: 'email', value: 'info@txowork.com'},],
+        [{type: 'colTitle', value: 'address'}, {type: 'title', value: 'morelands'}, {type: 'text', value: '5-23 Old Street'}, {type: 'text', value: 'London EC1V 9HL'},],
+        [{type: 'colTitle', value: 'connect'}, {type: 'link', value: 'instagram', url: 'www.instagram.com'}, {type: 'link', value: 'linkedin', url: 'www.linkedin.com'}, {type: 'link', value: 'facebook', url: 'www.facebook.com'},],
       ],
       introHeroText: 'We’re hands-on and committed to evolving your business to strive for better',
       introDescription: {title: 'Our offering', text: 'We provide physical, pragmatic and personal business guidance. Success is never static. Neither are we. We’ll be your business partners for positive change'},
-      carousel: [
+      carouselData: [
         {name: 'sample title', location: 'soho', availability: 'now', sizeValue: '4,200', sizeMeasure: 'sqft', description: 'Quam eos premqui tem cupta il inimet as rerum rent volum sitibus idunt la consenis ea nos doluptur, ipsapernates praeperrunte nobist peditaquis eum audaecto quam, susa consecae isto eum fugit.'},
         {name: 'sample title', location: 'soho', availability: 'now', sizeValue: '4,200', sizeMeasure: 'sqft', description: 'Quam eos premqui tem cupta il inimet as rerum rent volum sitibus idunt la consenis ea nos doluptur, ipsapernates praeperrunte nobist peditaquis eum audaecto quam, susa consecae isto eum fugit.'},
         {name: 'sample title', location: 'soho', availability: 'now', sizeValue: '4,200', sizeMeasure: 'sqft', description: 'Quam eos premqui tem cupta il inimet as rerum rent volum sitibus idunt la consenis ea nos doluptur, ipsapernates praeperrunte nobist peditaquis eum audaecto quam, susa consecae isto eum fugit.'},
