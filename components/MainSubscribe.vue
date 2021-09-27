878f120050111405765eebd5ede1532db6bd91f8
Ronald Greeff <ronald.greeff@gmail.com>
2021-09-27 10:57:15 +0100

    subscription component added

--- components/MainSubscribe.vue
+++ components/MainSubscribe.vue
@@ -2,7 +2,13 @@
   <div>
     <h2>Stay Updated</h2>
     <form>
-      <input></input>
+      <label for="full-name"></label>
+      <input type="text" id="full-name" name="full-name" placeholder="Full Name">
+      <label for="company-name"></label>
+      <input type="text" id="company-name" name="company-name" placeholder="Company Name">
+      <label for="email-address"></label>
+      <input type="text" id="email-address" name="email-address" placeholder="Email Address">
+      <input type="submit" value="Submit">
     </form>
   </div>
 </template>
