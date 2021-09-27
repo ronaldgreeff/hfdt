f24677111cc5973a16fbefff5f9e7f0c6a246b70
Ronald Greeff <ronald.greeff@gmail.com>
2021-09-27 11:00:18 +0100

    Nav components done

---
+++ components/NavContactBlock.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="" v-for="contactLine in contactDetail">
+      <p v-if="contactLine.value === ''">&nbsp;</p>
+      <p v-else-if="contactLine.type === 'title'">{{ contactLine.value | capitalize }}</p>
+      <a v-else-if="contactLine.type === 'email'" :href="'mailto:' + contactLine.value">{{ contactLine.value }}</a>
+      <a v-else-if="contactLine.type === 'link'" :href="'//' + contactLine.url" target="_blank">{{ contactLine.value | upperCase }}</a>
+      <p v-else>{{ contactLine.value }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    contactDetail: {
+      type: Array
+    }
+  },
+}
+</script>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,11 +1,41 @@
 <template>
-  <div>
-    <p
+  <div class="contact-block-parent">
+    <LogoTXo2
       v-gsap.from="{
         opacity: 0, 
         x: -200, 
         duration: 1
       }"
-    >navbar</p>
+    />
+    <div class="contact-block-child" v-for="contactDetail in navContacts">
+      <NavContactBlock v-bind:contactDetail="contactDetail" />
+    </div>
   </div>
 </template>
+
+<script>
+import LogoTXo2 from '@/components/LogoTXo2'
+import NavContactBlock from '@/components/NavContactBlock'
+
+export default {
+  components: {
+    LogoTXo2,
+    NavContactBlock,
+  },
+  props: {
+    navContacts: Array,
+  }
+}
+</script>
+
+<style>
+.contact-block-parent
+{
+  display: table;
+  width: 100%;
+}
+.contact-block-child
+{
+  display: table-cell;
+}
+</style>
