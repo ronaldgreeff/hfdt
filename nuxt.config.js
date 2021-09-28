3b30057f311734b3c775437d0c09ad7375865a3b
Ronald Greeff <ronald.greeff@gmail.com>
2021-09-28 16:11:07 +0100

    minicss added

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~layouts/global.css',
+    'mini.css',
+    '~layouts/global.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
