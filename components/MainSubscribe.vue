e8946fa67af7e25518c4139a93af3f1b835c7463
Ronald Greeff <ronald.greeff@gmail.com>
2021-09-30 17:30:56 +0100

    MainSubscribe looks good

--- components/MainSubscribe.vue
+++ components/MainSubscribe.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
     <h2>STAY UPDATED</h2>
+    <p class="newsletter-text">Newsletter</p>
     <form>
-      <label for="full-name"></label>
-      <input type="text" id="full-name" name="full-name" placeholder="Full Name">
-      <label for="company-name"></label>
-      <input type="text" id="company-name" name="company-name" placeholder="Company Name">
-      <label for="email-address"></label>
-      <input type="text" id="email-address" name="email-address" placeholder="Email Address">
-      <input type="submit" value="Submit">
+      <label class="hidden" for="full-name"></label>
+      <input class="newsletter-text" type="text" id="full-name" name="full-name" placeholder="Full Name">
+      <label class="hidden" for="company-name"></label>
+      <input class="newsletter-text" type="text" id="company-name" name="company-name" placeholder="Company Name">
+      <label class="hidden" for="email-address"></label>
+      <input class="newsletter-text" type="text" id="email-address" name="email-address" placeholder="Email Address">
+      <button class="submit" type="submit">Submit</button>
     </form>
   </div>
 </template>
