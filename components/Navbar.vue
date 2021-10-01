83408f38a186fc3e3690fc33877cc5a245f28a31
ronaldgreeff <ronald.greeff@gmail.com>
2021-10-01 08:14:59 +0100

    navbar collapses, looks ok on mobile

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,46 +1,40 @@
 <template>
-  <div>
+  <header id="header">
+    <nav class="header">
 
-  <header class="header hidden">
-    <a href="" class="logo">CSS Nav</a>
-    <input class="menu-btn" type="checkbox" id="menu-btn" />
-    <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
-    <ul class="menu">
-      <li><a href="#work">Our Work</a></li>
-      <li><a href="#about">About</a></li>
-      <li><a href="#careers">Careers</a></li>
-      <li><a href="#contact">Contact</a></li>
-    </ul>
-  </header>
+      <a href="/" class="logo">
+        <LogoTXo2
+          v-gsap.from="{
+            opacity: 0,
+            x: -200,
+            y: 0,
+            duration: 1
+          }"
+        />
+      </a>
 
-  <nav>
-    <div class="nav-logo">
-      <LogoTXo2
-        v-gsap.from="{
-          opacity: 0, 
-          x: -200,
-          duration: 1
-        }"
-      />
-    </div>
-    <div class="spacer"></div>
-    <hr>
-    <div class="nav-contacts">
-      <div class="nav-contact-col" v-for="navContactCol in navColumns">
-        <h3 class="nav-contact-row-title">{{ navContactCol.colTitle | upperCase }}</h3>
-        <div class="nav-contact-row" v-for="navContactRow in navContactCol.contactBlocks">
-          <div class="nav-contact-line" v-for="contactLine in navContactRow">
-            <p class="reduce-y-margin" v-if="contactLine.type === 'title'">{{ contactLine.value | capitalize }}</p>
-            <a class="reduce-y-margin" v-else-if="contactLine.type === 'email'" :href="'mailto:' + contactLine.value">{{ contactLine.value | capitalize }}</a>
-            <a class="reduce-y-margin" v-else-if="contactLine.type === 'link'" :href="'//' + contactLine.url" target="_blank">{{ contactLine.value | capitalize }}</a>
-            <p class="reduce-y-margin" v-else>{{ contactLine.value }}</p>
+      <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+      <input class="menu-btn" type="checkbox" id="menu-btn" />
+
+      <div class="spacer"></div>
+      <hr>
+
+      <ul class="menu nav-contacts">
+        <li class="nav-contact-col" v-for="navContactCol in navColumns">
+          <h3 class="nav-contact-row-title">{{ navContactCol.colTitle | upperCase }}</h3>
+          <div class="nav-contact-row" v-for="navContactRow in navContactCol.contactBlocks">
+            <div class="nav-contact-line" v-for="contactLine in navContactRow">
+              <p class="reduce-y-margin" v-if="contactLine.type === 'title'">{{ contactLine.value | capitalize }}</p>
+              <a class="reduce-y-margin" v-else-if="contactLine.type === 'email'" :href="'mailto:' + contactLine.value">{{ contactLine.value | capitalize }}</a>
+              <a class="reduce-y-margin" v-else-if="contactLine.type === 'link'" :href="'//' + contactLine.url" target="_blank">{{ contactLine.value | capitalize }}</a>
+              <p class="reduce-y-margin" v-else>{{ contactLine.value }}</p>
+            </div>
           </div>
-        </div>
-        <hr>
-      </div>
-    </div>
-  </nav>
-  </div>
+          <hr>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <script>
@@ -58,27 +52,20 @@ export default {
 
 <style>
 
-nav {
+/* nav {
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
-}
+} */
 
-.nav-logo {width: 100%;}
-.nav-contacts
-{
-  /* max-width: 50%; */
-}
+/* .nav-logo {width: 100%;} */
+/* .nav-contacts { max-width: 50%; } */
 
-.contact-block-child
-{
-  display: table-cell;
-}
-/*  */
+.contact-block-child { display: table-cell; }
+
 .header {
-  background-color: #fff;
-  box-shadow: 1px 1px 4px 0 rgba(0,0,0,.1);
-  position: fixed;
+
+  position: relative;
   width: 100%;
   z-index: 3;
 }
@@ -88,20 +75,14 @@ nav {
   padding: 0;
   list-style: none;
   overflow: hidden;
-  background-color: #fff;
 }
 
-.header li a {
-  display: block;
-  padding: 20px 20px;
-  border-right: 1px solid #f4f4f4;
-  text-decoration: none;
-}
+.header li { display: block; }
 
-.header li a:hover,
-.header .menu-btn:hover {
+/* .header li a:hover, */
+/* .header .menu-btn:hover {
   background-color: #f4f4f4;
-}
+} */
 
 .header .logo {
   display: block;
@@ -112,7 +93,6 @@ nav {
 }
 
 /* menu */
-
 .header .menu {
   clear: both;
   max-height: 0;
@@ -120,7 +100,6 @@ nav {
 }
 
 /* menu icon */
-
 .header .menu-icon {
   cursor: pointer;
   display: inline-block;
@@ -132,7 +111,7 @@ nav {
 
 .header .menu-icon .navicon {
   background: #333;
-  display: block;
+  display: inline-block;
   height: 2px;
   position: relative;
   transition: background .2s ease-out;
@@ -165,7 +144,7 @@ nav {
 }
 
 .header .menu-btn:checked ~ .menu {
-  max-height: 240px;
+  max-height: 100%;
 }
 
 .header .menu-btn:checked ~ .menu-icon .navicon {
